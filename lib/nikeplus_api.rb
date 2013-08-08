3edc4629ad6e33092b7a3723a71e058e664d761d
AKovtunov <1sasha.sasha1@gmail.com>
2013-08-08 10:35:47 +0300

    Refactored methods

--- lib/nikeplus_api.rb
+++ lib/nikeplus_api.rb
@@ -12,46 +12,33 @@ module NikeplusApi
     HEADERS = {"appid"=>CONFIG["APPID"], "Accept"=>"application/json", "Content-Type"=>"application/json"}
 
     def initialize(token)
-      if token.nil?
-        raise ArgumentError, "You should input your generated token"
-      else
-        @token = token
-      end
-      @url = "https://api.nike.com/me/"
+      raise ArgumentError, "You should input your generated token" if token.nil?
+      @token = token
     end
 
     def get_data url
-      response = HTTParty.get(url, :headers => HEADERS)
+      base_url = "https://api.nike.com/me/"
+      response = HTTParty.get(base_url+url, :headers => HEADERS)
       return JSON.parse(response.body)
     end
 
 
     def list_activities
-      url=@url+"sport/activities?access_token=#{token}"
-      get_data url
+      get_data "sport/activities?access_token=#{token}"
     end
 
     def aggregate_sport_data
-      url=@url+"sport?access_token=#{token}"
-      get_data url
+      get_data "sport?access_token=#{token}"
     end
 
     def activity_details id
-	    if id.nil?
-	     raise ArgumentError, "You should input id of activity"
-      else
-        url=@url+"sport/activities/#{id}?access_token=#{token}"
-        get_data url
-      end
+      raise ArgumentError, "You should input id of activity" if id.nil?
+      get_data "sport/activities/#{id}?access_token=#{token}"
     end
 
     def gps_data id
-      if id.nil?
-        raise ArgumentError, "You should input id of activity"
-      else
-        url=@url+"sport/activities/#{id}/gps?access_token=#{token}"
-        get_data url
-      end
+      raise ArgumentError, "You should input id of activity" if id.nil?
+      get_data "sport/activities/#{id}/gps?access_token=#{token}"
     end
 
   end
